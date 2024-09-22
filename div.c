d9262a9ac8ab83d6d891394cef215422886dd7dc
Mohamad Rami Karkour <129765038+RamiKarkour@users.noreply.github.com>
2024-09-22 22:44:50 +0200

    Update div.c

--- div.c
+++ div.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
+    if (argc != 3) 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
 
-  return 0;
+    char *endptr;
+    float num1 = strtod(argv[1], &endptr);
+    if (*endptr != '\0') 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
+
+    float num2 = strtod(argv[2], &endptr);
+    if (*endptr != '\0') 
+    {
+        printf("-ERROR-\n");
+        return 1;
+    }
+
+    if (num2 == 0.0) 
+    {
+        printf("DIV ZERO NOT ALLOWED!\n");
+        return 1;
+    }
+
+    float result = num1 / num2;
+    printf("%0.1f\n", result);
+
+    return 0;
 }
